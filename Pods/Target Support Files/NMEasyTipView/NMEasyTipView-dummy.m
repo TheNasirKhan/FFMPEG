db854f00772688ff6d42692b938d29be054f0bd4
nasir.khan <nasir.khan@innovativesolutions.ae>
2019-07-02 16:21:24 +0500

    Testing opus conversion

---
+++ Pods/Target Support Files/NMEasyTipView/NMEasyTipView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NMEasyTipView : NSObject
+@end
+@implementation PodsDummy_NMEasyTipView
+@end
---
+++ Pods/Target Support Files/NMEasyTipView/NMEasyTipView-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "RCEasyTipView.h"
+#import "RCEasyTipViewIDGenerator.h"
+#import "UIBarItem+RCBarItem.h"
+#import "UIView+RCEssentials.h"
+
+FOUNDATION_EXPORT double NMEasyTipViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char NMEasyTipViewVersionString[];
+
